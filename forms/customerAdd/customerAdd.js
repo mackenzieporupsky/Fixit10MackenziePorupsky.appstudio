52de149967c2ededcd1aa74214639bc93711c1dc
mackenzieporupsky <mack@porupsky.net>
2020-10-25 21:02:25 -0500

    form 3

--- forms/customerAdd/customerAdd.js
+++ forms/customerAdd/customerAdd.js
@@ -1,55 +1,35 @@
-/*
-let req = ""
-let query = ""
-let results = ""
-let pw = "BIA375pw"
-let userName = "mkp29690"
 
-customerSelect.onshow=function() { 
-  drpSelect.clear()
-  query = "SELECT name FROM customer" 
+req = ""
+query = ""
+results = ""
+
+btnAdd.onclick = function() {
+  query = "INSERT INTO customer VALUES ('17','Jesse Antiques','1113 F St','Omaha','NE','68178')"
   req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=mkp29690&pass=" + pw + "&database=mkp29690&query=" + query)
-  
+
   if (req.status == 200) { 
-    customerSelect = JSON.parse(req.responseText)
-    console.log(drpSelect)
+    if (req.responseText == 500) { 
+      console.log("You have successfully added the Customer!")
+    } else
+      console.log("There was a problem with adding the Customer to the database.")
+  } else {
+    console.log("Error: " + req.status);
   }
-  
-  if (customerSelect.length == 0) { 
-    console.log("There are no customers found.")
-  } else { 
-    for (i=0; i<= customerSelect.length - 1; i++)
-      drpSelect.addItem(customerSelect[i])
+
+  query = `SELECT name from customer ORDER BY customer_id DESC`
+  req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=mkp29690&pass=" + pw + "&database=mkp29690&query=" + query)
+
+  if (req.status == 200) { 
+    results = JSON.parse(req.responseText)
+  } else {
+    console.log(`Error: ${req.status}`);
   }
+  let customersAdd = ""
+  for (i = 0; i <= results.length - 1; i++)
+    customersAdd = customersAdd + results[i] + "\n"
+  txtAdd.value = customersAdd
 }
-      
-  drpSelect.onclick=function(s) { 
-    if (typeof(s) == "object") 
-      return 
-    else { 
-      console.log(s)
-      drpSelect.value = s
-      query = `SELECT state FROM customer WHERE name = '${s}'`
-      req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=mkp29690&pass=" + pw + "&database=mkp29690&query=" + query)
-      
-      if (req.status == 200) { 
-        customerState = JSON.parse(req.responseText)
-        console.log(customerState)
-      }
-      query = `SELECT name FROM customer WHERE state = '${customerState[0]}'`
-       req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=mkp29690&pass=" + pw + "&database=mkp29690&query=" + query)
-       
-       if (req.status == 200) { 
-        customerSameState = JSON.parse(req.responseText) 
-        console.log(customerSameState)
-      }
-      
-      
-      let userMessage = ""
-      for (i=0; i<= customerSameState.length - 1; i++) 
-          userMessage = userMessage + customerSameState[i] + "\n"
-          txtSelect.value=userMessage
-      }
-  }
+btnNext3.onclick=function(){
+  ChangeForm(customerUpdate)
+}
   
-*/
